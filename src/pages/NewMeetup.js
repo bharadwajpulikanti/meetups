ac76cb6ee18159c151c012fb904aaef62ab38542
bharadwajpulikanti <bharadwajpulikanti25@gmail.com>
2024-02-20 16:39:04 +0530

    Added Post Request code to the firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-3e1d4-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddNewMeetUp={addMeetupHandler} />
     </section>
   );
 }
